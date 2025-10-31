4266868c5a17211769039ba6640ee191ae0e15d1
Nguyentatho <nguyentatho14106@gmail.com>
2025-10-31 20:16:26 +0700

    'finally'

--- src/pages/blogpage/BlogPage.tsx
+++ src/pages/blogpage/BlogPage.tsx
@@ -7,7 +7,7 @@ export default function BlogPage() {
     return (
         <>
             {/* header: 90px footer: 160px */}
-            <div className="pt-22.5 pb-40 bg-[#D9D9D9]">
+            <div className=" bg-[#D9D9D9] pb-30">
                 {/* banner */}
                 <div className=" relative bg-[url('D:\TASK_clubs_wed\task2\src\assets\blog\bannerBlog.png')] bg-cover bg-center h-150 flex items-center justify-center text-white">
                     {/* Overlay */}
@@ -20,10 +20,10 @@ export default function BlogPage() {
                 </div>
 
                 {/* News post */}
-                <div className="p-16 flex flex-col gap-10">
+                <div className="px-16 pt-16 flex flex-col gap-10">
                     <h2 className="text-[40px] font-extrabold text-[#E91E63]">CÁC BÀI VIẾT MỚI NHẤT</h2>
                     {/* main card */}
-                    <div className="w-full flex ">
+                    <div className="w-full flex border border-[#333333] rounded-xl">
                         <img src={first_thirt} alt="image AWS Sập DNS DynamoDB Khu Vực Us-East-1" className='object-cover' />
                         <div className='py-6 px-10'>
                             <h3 className='text-[24px] font-semibold'>AWS Sập DNS DynamoDB Khu Vực Us-East-1</h3>
@@ -69,7 +69,7 @@ export default function BlogPage() {
                                     {/* Overlay mờ */}
                                     {/* <div className="absolute inset-0 bg-black/30 backdrop-blur-sm"></div> */}
 
-                                    <span className='mx-6 text-[#E91E63] font-medium'>Kiến Thức</span>
+                                    <p className='mx-6 text-[#E91E63] font-medium pt-1'>Kiến Thức</p>
                                 </div>
                             }
                         >
@@ -78,7 +78,7 @@ export default function BlogPage() {
                                 <h2 className='font-bold text-2xl'>Workshop Ứng Dụng AI Vào Học Tập</h2>
                                 <p className='text-sm text-gray-600 mt-2'>Ngày 11/8 vừa qua, Codeking và sinh viên PTIT đã chung tay hiện thực hóa mô hình đào tạo gắn kết chặt chẽ giữa môi trường...</p>
                                 <div className='flex justify-between items-center mt-8'>
-                                    <span>22/10/2025</span>
+                                    <span className='text-[12px] font-medium'>22/10/2025</span>
                                     <button className='w-32 h-10 bg-[#380320] text-white hover:text-black hover:bg-[#D9D9D9] rounded-3xl font-medium border'>Xem Thêm</button>
                                 </div>
                             </div>
@@ -101,7 +101,7 @@ export default function BlogPage() {
                                     {/* Overlay mờ */}
                                     {/* <div className="absolute inset-0 bg-black/30 backdrop-blur-sm"></div> */}
 
-                                    <span className='mx-6 text-[#E91E63] font-medium'>Kiến Thức</span>
+                                    <p className='mx-6 text-[#E91E63] font-medium pt-1'>Kiến Thức</p>
                                 </div>
                             }
                         >
@@ -110,7 +110,7 @@ export default function BlogPage() {
                                 <h2 className='font-bold text-2xl'>Workshop Ứng Dụng AI Vào Học Tập</h2>
                                 <p className='text-sm text-gray-600 mt-2'>Ngày 11/8 vừa qua, Codeking và sinh viên PTIT đã chung tay hiện thực hóa mô hình đào tạo gắn kết chặt chẽ giữa môi trường...</p>
                                 <div className='flex justify-between items-center mt-8'>
-                                    <span>22/10/2025</span>
+                                    <span className='text-[12px] font-medium'>22/10/2025</span>
                                     <button className='w-32 h-10 bg-[#380320] text-white hover:text-black hover:bg-[#D9D9D9] rounded-3xl font-medium border'>Xem Thêm</button>
                                 </div>
                             </div>
@@ -133,7 +133,7 @@ export default function BlogPage() {
                                     {/* Overlay mờ */}
                                     {/* <div className="absolute inset-0 bg-black/30 backdrop-blur-sm"></div> */}
 
-                                    <span className='mx-6 text-[#E91E63] font-medium'>Kiến Thức</span>
+                                    <p className='mx-6 text-[#E91E63] font-medium pt-1'>Kiến Thức</p>
                                 </div>
                             }
                         >
@@ -142,13 +142,11 @@ export default function BlogPage() {
                                 <h2 className='font-bold text-2xl'>Workshop Ứng Dụng AI Vào Học Tập</h2>
                                 <p className='text-sm text-gray-600 mt-2'>Ngày 11/8 vừa qua, Codeking và sinh viên PTIT đã chung tay hiện thực hóa mô hình đào tạo gắn kết chặt chẽ giữa môi trường...</p>
                                 <div className='flex justify-between items-center mt-8'>
-                                    <span>22/10/2025</span>
+                                    <span className='text-[12px] font-medium'>22/10/2025</span>
                                     <button className='w-32 h-10 bg-[#380320] text-white hover:text-black hover:bg-[#D9D9D9] rounded-3xl font-medium border'>Xem Thêm</button>
                                 </div>
                             </div>
                         </Card>
-
-
                     </div>
                 </div>
 
@@ -168,24 +166,11 @@ export default function BlogPage() {
                         <div className="w-full pl-14 pr-10 flex ">
                             <img src={first_thirt} alt="image AWS Sập DNS DynamoDB Khu Vực Us-East-1" className='object-cover' />
                             <div className='py-6 px-10'>
+                                <p className='font-medium text-[#333333] mb-2 '>22/10/2025</p>
                                 <h3 className='text-[24px] font-semibold'>AWS Sập DNS DynamoDB Khu Vực Us-East-1</h3>
                                 <div className='mt-4  text-[#333333]'>
                                     <p>Ngày 20/10/2025, nhiều tổ chức công nghệ toàn cầu ghi nhận lỗi API, độ trễ tăng cao và dịch vụ ngừng phản hồi do sự cố tại vùng US-EAST-1 của Amazon Web Services (AWS) trung tâm hạ tầng quan trọng bậc nhất của internet hiện nay...</p>
-                                    <div className='mt-10 flex gap-7'>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Danh mục</span>
-                                            <span className='font-medium'>Công nghệ</span>
-                                        </div>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Ngày đăng</span>
-                                            <span className='font-medium'>20/10/2025</span>
-                                        </div>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Tác giả</span>
-                                            <span className='font-medium'>Lê Phú Thịnh</span>
-                                        </div>
-                                    </div>
-                                    <div className='flex justify-end'>
+                                    <div className='flex justify-end mt-8'>
                                         <button className='w-42 h-10 border-black-500 border rounded-3xl end hover:bg-gray-700 hover:text-white'>Xem Thêm</button>
                                     </div>
                                 </div>
@@ -196,24 +181,11 @@ export default function BlogPage() {
                         <div className="w-full pl-14 pr-10 flex ">
                             <img src={first_thirt} alt="image AWS Sập DNS DynamoDB Khu Vực Us-East-1" className='object-cover' />
                             <div className='py-6 px-10'>
+                                <p className='font-medium text-[#333333] mb-2'>22/10/2025</p>
                                 <h3 className='text-[24px] font-semibold'>AWS Sập DNS DynamoDB Khu Vực Us-East-1</h3>
                                 <div className='mt-4  text-[#333333]'>
                                     <p>Ngày 20/10/2025, nhiều tổ chức công nghệ toàn cầu ghi nhận lỗi API, độ trễ tăng cao và dịch vụ ngừng phản hồi do sự cố tại vùng US-EAST-1 của Amazon Web Services (AWS) trung tâm hạ tầng quan trọng bậc nhất của internet hiện nay...</p>
-                                    <div className='mt-10 flex gap-7'>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Danh mục</span>
-                                            <span className='font-medium'>Công nghệ</span>
-                                        </div>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Ngày đăng</span>
-                                            <span className='font-medium'>20/10/2025</span>
-                                        </div>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Tác giả</span>
-                                            <span className='font-medium'>Lê Phú Thịnh</span>
-                                        </div>
-                                    </div>
-                                    <div className='flex justify-end'>
+                                    <div className='flex justify-end mt-8'>
                                         <button className='w-42 h-10 border-black-500 border rounded-3xl end hover:bg-gray-700 hover:text-white'>Xem Thêm</button>
                                     </div>
                                 </div>
@@ -224,32 +196,31 @@ export default function BlogPage() {
                         <div className="w-full pl-14 pr-10 flex ">
                             <img src={first_thirt} alt="image AWS Sập DNS DynamoDB Khu Vực Us-East-1" className='object-cover' />
                             <div className='py-6 px-10'>
+                                <p className='font-medium text-[#333333] mb-2'>22/10/2025</p>
                                 <h3 className='text-[24px] font-semibold'>AWS Sập DNS DynamoDB Khu Vực Us-East-1</h3>
                                 <div className='mt-4  text-[#333333]'>
                                     <p>Ngày 20/10/2025, nhiều tổ chức công nghệ toàn cầu ghi nhận lỗi API, độ trễ tăng cao và dịch vụ ngừng phản hồi do sự cố tại vùng US-EAST-1 của Amazon Web Services (AWS) trung tâm hạ tầng quan trọng bậc nhất của internet hiện nay...</p>
-                                    <div className='mt-10 flex gap-7'>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Danh mục</span>
-                                            <span className='font-medium'>Công nghệ</span>
-                                        </div>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Ngày đăng</span>
-                                            <span className='font-medium'>20/10/2025</span>
-                                        </div>
-                                        <div className='flex flex-col'>
-                                            <span className='text-[#333333]'>Tác giả</span>
-                                            <span className='font-medium'>Lê Phú Thịnh</span>
-                                        </div>
-                                    </div>
-                                    <div className='flex justify-end'>
+                                    <div className='flex justify-end mt-8'>
                                         <button className='w-42 h-10 border-black-500 border rounded-3xl end hover:bg-gray-700 hover:text-white'>Xem Thêm</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className='py-7 flex items-center justify-center gap-3 border-[#333333] border-b-2'>
+                        <span className='text-gray-500 flex justify-center items-center font-bold hover:text-black hover:bg-[#E91E63] hover:w-10 hover:h-10 hover:rounded-full active:bg-[#4d071e] cursor-pointer'>&lt;</span>
+                        <div className='flex gap-3'>
+                            <button className='w-10 h-10 cursor-pointer font-bold rounded-lg bg-[#E91E63]'>1</button>
+                            <button className='font-bold cursor-pointer hover:bg-[#E91E63] hover:w-10 hover:h-10 active:bg-[#4d071e]'>2</button>
+                            <button className='font-bold cursor-pointer hover:bg-[#E91E63] hover:w-10 hover:h-10 active:bg-[#4d071e]'>3</button>
+                            <button className='font-bold cursor-pointer hover:bg-[#E91E63] hover:w-10 hover:h-10 active:bg-[#4d071e]'>4</button>
+                            <button className='font-bold '>...</button>
+                            <button className='font-bold cursor-pointer hover:bg-[#E91E63] hover:w-10 hover:h-10 active:bg-[#4d071e]'>10</button>
+                        </div>
+                        <span className='text-gray-500 flex justify-center items-center font-bold hover:text-black hover:bg-[#E91E63] hover:w-10 hover:h-10 hover:rounded-full active:bg-[#4d071e] cursor-pointer'>&gt;</span>
                     </div>
                 </div>
-
+                    <div></div>
 
 
             </div>
